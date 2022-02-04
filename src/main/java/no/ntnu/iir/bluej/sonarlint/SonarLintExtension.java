7d91d7ab5d5b888be2ebad4ab1ffbcc9dc889841
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-02-04 07:52:52 +0100

    feat: add version validation in compatibility check + url

--- src/main/java/no/ntnu/iir/bluej/sonarlint/SonarLintExtension.java
+++ src/main/java/no/ntnu/iir/bluej/sonarlint/SonarLintExtension.java
@@ -2,6 +2,7 @@ package no.ntnu.iir.bluej.sonarlint;
 
 import bluej.extensions2.BlueJ;
 import bluej.extensions2.Extension;
+import java.net.URL;
 import no.ntnu.iir.bluej.extensions.linting.core.handlers.FilesChangeHandler;
 import no.ntnu.iir.bluej.extensions.linting.core.handlers.PackageEventHandler;
 import no.ntnu.iir.bluej.extensions.linting.core.ui.AuditWindow;
@@ -35,12 +36,23 @@ public class SonarLintExtension extends Extension {
 
   @Override
   public boolean isCompatible() {
-    return true;
+    int versionMajor = Extension.getExtensionsAPIVersionMajor();
+    int versionMinor = Extension.getExtensionsAPIVersionMinor();
+    return (versionMajor == 3 && versionMinor == 2);
   }
 
   @Override
   public String getVersion() {
     return this.getClass().getPackage().getImplementationVersion();
+  }
+
+  @Override
+  public URL getURL() {
+    try {
+      return new URL("https://github.com/NTNU-IE-IIR/BlueJ-SonarLint-Plugin/");
+    } catch (Exception e) {
+      return null;
+    }
   }
 
   @Override
